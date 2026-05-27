29982583f809f4f160bcb63c5ea5b23ffac9a7fe
kevinfiol <me@kevinfiol.com>
2026-05-27 16:41:44 -0400

    add base cli code and models file

--- packages/cli/src/cli/main.py
+++ packages/cli/src/cli/main.py
@@ -1,10 +1,20 @@
+import argparse
+
 from core import diff_xml
+from core.models import DiffingOptions
 
 
 def main() -> None:
     """CLI entrypoint."""
-    xml = diff_xml()
-    print(xml)
+    ap = argparse.ArgumentParser(description="Diff two CDA/eICR XML files.")
+    ap.add_argument("file1", help="Original CDA/eICR XML (before)")
+    ap.add_argument("file2", help="New CDA/eICR XML (after)")
+
+    args = ap.parse_args()
+    opts = DiffingOptions(**vars(args))
+
+    _xml = diff_xml(opts)
+    # print(_xml)
 
 
 if __name__ == "__main__":
