50d5a5e9e8424c1ef49b911f4c32dc13cef0ad19
kevinfiol <me@kevinfiol.com>
2026-05-28 16:54:09 -0400

    wip

--- packages/core/src/core/__init__.py
+++ packages/core/src/core/__init__.py
@@ -4,6 +4,8 @@ from typing import Any
 
 from lxml import etree
 
+from core.xml_utils import build_standalone_xml_string
+
 from .constants import HL7_NAMESPACE
 from .diff_engine import collect_additions_updates_deletes
 from .models import DiffingOptions
@@ -17,8 +19,11 @@ MODE = "WATCH"
 #     "//hl7:observation[@moodCode = 'EVN']/hl7:value/@displayName",
 # ]
 
+# what if i watched for hl7:value, and it *is* added, but deeply nested somewhere?
+
 XPATHS = [
     "/hl7:ClinicalDocument/hl7:component/hl7:structuredBody/hl7:component/hl7:section//hl7:value",
+    # "/hl7:ClinicalDocument/hl7:component/hl7:structuredBody/hl7:component/hl7:section//hl7:value",
     # "/hl7:ClinicalDocument/hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:text",
 ]
 
@@ -27,22 +32,23 @@ def eval_xpath(elem: etree._Element | etree._ElementTree, xpath_expr: str) -> li
     return elem.xpath(xpath_expr, namespaces=HL7_NAMESPACE) or []
 
 
-def build_watched(elem: etree._ElementTree):
-    nodes = {}
-    # attrs = {}
+def node_or_child_in_map(
+    elem: etree._Element, list: dict[etree._Element, dict[str, str]]
+) -> etree._Element | None:
+    if elem in list:
+        return elem
+    for x in elem.iterdescendants():
+        if x in list:
+            return x
+    return None
 
+
+def build_watched(elem: etree._ElementTree) -> dict[etree._Element, dict[str, str]]:
+    nodes: dict[etree._Element, dict[str, str]] = {}
     for xpath in XPATHS:
         vals = eval_xpath(elem, xpath)
-
         for val in vals:
             nodes[val] = {"tag": val.tag}
-            # is_attribute = getattr(val, "is_attribute", False)
-
-            # if is_attribute:
-            #     parent = val.getparent()
-            #     attrs[parent] = {"name": val.attrname}
-            # else:
-
     return nodes
 
 
@@ -62,13 +68,28 @@ def diff_xml(opts: DiffingOptions) -> str:
         tree_left.getroot(), tree_right.getroot()
     )
 
-    for u in updated:
-        before, after = u
+    for x in added:
+        added_node = node_or_child_in_map(x, watched_right_nodes)
+        if isinstance(added_node, etree._Element):
+            print("a watched xpath has been added")
+            # TODO: maybe good to include the *actual* added parent element
+            print(build_standalone_xml_string(added_node))
+            print(tree_right.getelementpath(added_node))
 
-        if before in watched_left_nodes:
-            print("this is being watched in the left tree")
-        if after in watched_right_nodes:
-            print("this is being watched in the right tree")
+    # for deleted_node in deleted:
+    #     if node_or_child_in_map(deleted_node, watched_left_nodes):
+    #         print("yes that node was in the first tree")
+    #         if not node_or_child_in_map(deleted_node, watched_right_nodes):
+    #             print("and is no longer in the right tree")
+    #         print(build_standalone_xml_string(deleted_node))
+
+    # for u in updated:
+    #     before, after = u
+
+    #     if before in watched_left_nodes:
+    #         print("this is being watched in the left tree")
+    #     if after in watched_right_nodes:
+    #         print("this is being watched in the right tree")
 
     # if left in watched_left_attrs:
     #     print("its watched")
