7f3bcb58a9c993fbe968d294978a45667af2a2ee
kevinfiol <me@kevinfiol.com>
2026-06-15 13:43:04 -0400

    json output in CLI

--- packages/cli/src/cli/main.py
+++ packages/cli/src/cli/main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from datetime import UTC, datetime
 from pathlib import Path
 from uuid import uuid4
 
@@ -13,11 +14,19 @@ DEFAULT_CONFIG_PATH = Path(__file__).parent / "default_config.json"
 
 def main() -> None:
     """CLI entrypoint."""
+    timestamp = datetime.now(UTC).strftime("%Y-%m-%dT%H-%M-%SZ")
+
     ap = argparse.ArgumentParser(description="Diff two CDA/eICR XML files.")
     ap.add_argument("file1", help="Original CDA/eICR XML (before)")
     ap.add_argument("file2", help="New CDA/eICR XML (after)")
     ap.add_argument(
         "-c", "--config", help="Path to configuration", default=str(DEFAULT_CONFIG_PATH)
+    )
+    ap.add_argument(
+        "-o",
+        "--output-diff-file",
+        help="Path to output diff JSON",
+        default=f"{timestamp}_diff_output.json",
     )
 
     args = ap.parse_args()
@@ -28,7 +37,11 @@ def main() -> None:
 
     diff_output = diff_xml(opts, config)
     diff_output_json = diff_output.model_dump_json(indent=2)
-    print(diff_output_json)
+
+    if opts.output_diff_file:
+        json_out_path = Path(opts.output_diff_file)
+        json_out_path.write_text(diff_output_json, encoding="utf-8")
+        print(f"Wrote {json_out_path.resolve()}")
 
     # TODO: refactor so XML is only parsed once
     parser = etree.XMLParser(remove_blank_text=True, huge_tree=True)
@@ -43,7 +56,8 @@ def main() -> None:
         augmentation_run,
         jurisdiction_id=jurisdiction_id,
         condition_grouper_uuid=condition_grouper_uuid,
-        diff_output=diff_output)
+        diff_output=diff_output,
+    )
 
     output_bytes = etree.tostring(
         eicr_root,
@@ -55,9 +69,9 @@ def main() -> None:
     print(f"Original document id: {augmented_eicr_result.original_doc_id}")
     print(f"Augmented document id: {augmented_eicr_result.augmented_doc_id}")
 
-    out_path = Path("augmented_eicr_with_diff.xml")
-    out_path.write_bytes(output_bytes)
-    print(f"Wrote {out_path.resolve()} ({len(output_bytes)} bytes)")
+    eicr_out_path = Path("augmented_eicr_with_diff.xml")
+    eicr_out_path.write_bytes(output_bytes)
+    print(f"Wrote {eicr_out_path.resolve()} ({len(output_bytes)} bytes)")
 
 
 if __name__ == "__main__":
