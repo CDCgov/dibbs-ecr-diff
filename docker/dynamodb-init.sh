c6b87e3b83a10b31b1d96f4a462ee40273397a7c
kevinfiol <me@kevinfiol.com>
2026-07-10 15:20:24 -0400

    wip

---
+++ docker/dynamodb-init.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# this script is run as a LocalStack init hook (specified in compose.yml)
+# see: https://docs.localstack.cloud/aws/customization/advanced/initialization-hooks/
+
+INIT_MARKER="/var/lib/localstack/dynamodb-init"
+TABLE_NAME="did-eicr-record"
+
+if [[ -f "${INIT_MARKER}" ]]; then
+  echo "DynamoDB init already completed; skipping."
+  exit 0
+fi
+
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-test}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-test}"
+export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-east-1}"
+
+# DynamoDB table creation
+# https://docs.aws.amazon.com/cli/latest/userguide/cli_dynamodb_code_examples.html
+if ! awslocal dynamodb describe-table --table-name "${TABLE_NAME}" >/dev/null 2>&1; then
+  awslocal dynamodb create-table \
+    --table-name "${TABLE_NAME}" \
+    --attribute-definitions \
+    AttributeName=setId,AttributeType=S \
+    AttributeName=versionNumber,AttributeType=N \
+    --key-schema \
+    AttributeName=setId,KeyType=HASH \
+    AttributeName=versionNumber,KeyType=RANGE \
+    --billing-mode PAY_PER_REQUEST \
+    >/dev/null
+fi
+
+# write marker file to denote that DynamoDB has been initialized
+echo "DynamoDB Intitialization Complete."
+touch "${INIT_MARKER}"
---
+++ docker/s3-init.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# this script is run as a LocalStack init hook (specified in compose.yml)
+# see: https://docs.localstack.cloud/aws/customization/advanced/initialization-hooks/
+
+INIT_MARKER="/var/lib/localstack/s3-init"
+
+INPUT_BUCKET="did-eicr-bucket"
+DIFF_BUCKET="did-diff-bucket"
+QUEUE_NAME="did-eicr-events"
+
+if [[ -f "${INIT_MARKER}" ]]; then
+  echo "S3/SQS init already completed; skipping."
+  exit 0
+fi
+
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-test}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-test}"
+# export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-"us-east-1"}"
+
+# create S3 buckets
+# https://docs.aws.amazon.com/cli/latest/userguide/cli_s3_code_examples.html
+for bucket in "${INPUT_BUCKET}" "${DIFF_BUCKET}"; do
+  if ! awslocal s3api head-bucket --bucket "${bucket}" >/dev/null 2>&1; then
+    awslocal s3api create-bucket --bucket "${bucket}"
+  fi
+done
+
+# create SQS queue for "did-eicr-bucket"
+# https://docs.aws.amazon.com/cli/latest/userguide/cli_sqs_code_examples.html
+
+if awslocal sqs get-queue-url --queue-name "${QUEUE_NAME}" >/dev/null 2>&1; then
+  # if the queue already exists, grab the queue url
+  QUEUE_URL="$(
+    awslocal sqs get-queue-url \
+      --queue-name "${QUEUE_NAME}" \
+      --query QueueUrl \
+      --output text
+  )"
+else
+  # else create it, and return queue url
+  QUEUE_URL="$(
+    awslocal sqs create-queue \
+      --queue-name "${QUEUE_NAME}" \
+      --query QueueUrl \
+      --output text
+  )"
+fi
+
+QUEUE_ARN="$(
+  awslocal sqs get-queue-attributes \
+    --queue-url "${QUEUE_URL}" \
+    --attribute-names QueueArn \
+    --query "Attributes.QueueArn" \
+    --output text
+)"
+
+# set SQS policy allowing S3 to publish events:
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/grant-destinations-permissions-to-s3.html
+POLICY="$(
+  cat <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "S3NotificationQueuePolicy",
+  "Statement": [
+    {
+      "Sid": "AllowS3ToSendMessages",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Action": [
+        "SQS:SendMessage"
+      ],
+      "Resource": "${QUEUE_ARN}",
+      "Condition": {
+        "ArnLike": {
+          "aws:SourceArn": "arn:aws:s3:::${INPUT_BUCKET}"
+        },
+        "StringEquals": {
+          "aws:SourceAccount": "000000000000"
+        }
+      }
+    }
+  ]
+}
+EOF
+)"
+
+awslocal sqs set-queue-attributes \
+  --queue-url "${QUEUE_URL}" \
+  --attributes Policy="$POLICY"
+
+# set configuration that specifies: when s3 objects are created, send to SQS
+# https://docs.aws.amazon.com/AmazonS3/latest/API/API_QueueConfiguration.html
+NOTIFICATION_CONFIGURATION="$(
+  cat <<EOF
+{
+  "QueueConfigurations": [
+    {
+      "Id": "SendObjectCreatedEventsToSqs",
+      "QueueArn": "${QUEUE_ARN}",
+      "Events": [
+        "s3:ObjectCreated:*"
+      ]
+    }
+  ]
+}
+EOF
+)"
+
+awslocal s3api put-bucket-notification-configuration \
+  --bucket "${INPUT_BUCKET}" \
+  --notification-configuration "${NOTIFICATION_CONFIGURATION}"
+
+# write marker file to denote that S3 + SQS have been initialized
+echo "S3 + SQS Intitialization Complete."
+touch "${INIT_MARKER}"
