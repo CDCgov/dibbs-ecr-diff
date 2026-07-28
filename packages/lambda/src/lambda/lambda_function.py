be8496b957491d1aa5d0530ec1881d3fd6364c22
Daniel Sass <DanielSass@users.noreply.github.com>
2026-07-28 14:04:20 -0400

    Dan/lambda logging (#88)
    * add placeholder logging
    
    * updating uv version

--- packages/lambda/src/lambda/lambda_function.py
+++ packages/lambda/src/lambda/lambda_function.py
@@ -1,11 +1,14 @@
+from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 
+logger = Logger(service="difference-in-docs")
 # TODO: use event source data class for event here
 # https://docs.aws.amazon.com/powertools/python/latest/utilities/data_classes/
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     """Main Lambda handler function."""
     try:
+        logger.info("hello world")
         return {
             "statusCode": 200,
             "message": "diff processed successfully",
