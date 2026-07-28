ca44120f6ee4b3830ed11bbad03befd53a449697
kevinfiol <me@kevinfiol.com>
2026-07-28 15:22:22 -0400

    fix ruff issues in our codebase

--- packages/lambda/src/lambda/lambda_function.py
+++ packages/lambda/src/lambda/lambda_function.py
@@ -1,8 +1,9 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
-
 logger = Logger(service="difference-in-docs")
+
+
 # TODO: use event source data class for event here
 # https://docs.aws.amazon.com/powertools/python/latest/utilities/data_classes/
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
