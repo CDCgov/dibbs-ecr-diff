eaa06c61cae404f31b00fde033e673ddae7c071d
kevinfiol <me@kevinfiol.com>
2026-08-21 18:17:39 -0400

    types for e2e tests

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -3,10 +3,13 @@
 import os
 from collections.abc import Iterator
 from pathlib import Path
-from typing import Any
 
 import boto3
 import pytest
+from types_boto3_dynamodb import DynamoDBClient
+from types_boto3_dynamodb.service_resource import DynamoDBServiceResource, Table
+from types_boto3_s3 import S3Client
+from types_boto3_s3.service_resource import S3ServiceResource
 
 from e2e.docker import run_compose
 from e2e.helpers import Uploader
@@ -36,7 +39,7 @@ def compose_stack() -> Iterator[None]:
 
 
 @pytest.fixture
-def s3_resource() -> Any:
+def s3_resource() -> S3ServiceResource:
     """S3 resource pre-configured to send requests to LocalStack."""
     return boto3.resource(
         "s3",
@@ -48,13 +51,13 @@ def s3_resource() -> Any:
 
 
 @pytest.fixture
-def s3(s3_resource: Any) -> Any:
+def s3(s3_resource: S3ServiceResource) -> S3Client:
     """S3 client pre-configured to send requests to LocalStack."""
     return s3_resource.meta.client
 
 
 @pytest.fixture
-def dynamodb_resource() -> Any:
+def dynamodb_resource() -> DynamoDBServiceResource:
     """DynamoDB resource pre-configured for LocalStack."""
     return boto3.resource(
         "dynamodb",
@@ -66,19 +69,19 @@ def dynamodb_resource() -> Any:
 
 
 @pytest.fixture
-def dynamodb(dynamodb_resource: Any) -> Any:
+def dynamodb(dynamodb_resource: DynamoDBServiceResource) -> DynamoDBClient:
     """DynamoDB client pre-configured for LocalStack."""
     return dynamodb_resource.meta.client
 
 
 @pytest.fixture
-def dynamodb_table(dynamodb_resource: Any) -> Any:
+def dynamodb_table(dynamodb_resource: DynamoDBServiceResource) -> Table:
     """DynamoDB table used by the e2e test suite."""
     return dynamodb_resource.Table(DYNAMODB_TABLE)
 
 
 @pytest.fixture
-def uploader(s3: Any, dynamodb: Any) -> Uploader:
+def uploader(s3: S3Client, dynamodb: DynamoDBClient) -> Uploader:
     """Build an Manifest builder + Uploader. Analog to docker/uploader.html."""
     uploader = Uploader(s3, BUCKET_NAME, ASSETS_DIR)
     uploader.wait_until_ready()
@@ -93,7 +96,7 @@ def uploader(s3: Any, dynamodb: Any) -> Uploader:
 
 @pytest.fixture(autouse=True)
 def clean_localstack_state(
-    uploader: Uploader, s3_resource: Any, dynamodb_table: Any
+    uploader: Uploader, s3_resource: S3ServiceResource, dynamodb_table: Table
 ) -> None:
     """Empties S3 and DynamoDB before each e2e test."""
     s3_resource.Bucket(uploader.bucket_name).objects.all().delete()
--- e2e/e2e.py
+++ e2e/e2e.py
@@ -1,9 +1,10 @@
-from typing import Any
+from types_boto3_s3 import S3Client
 
 from e2e.helpers import Pair, Uploader
 
 
-def test_happy_path(uploader: Uploader, s3: Any) -> None:
+def test_happy_path(uploader: Uploader, s3: S3Client) -> None:
+    """Happy path test case."""
     manifest = uploader.send_manifest("happy-path", [Pair(1), Pair(2), Pair(3)])
 
     did_complete_key = f"DIDCompleteV2/{manifest.persistence_id}"
