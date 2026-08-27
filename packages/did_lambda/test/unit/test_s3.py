e834886a7ced7739ffd930b3d08ef20de81312ea
kevinfiol <me@kevinfiol.com>
2026-08-27 15:53:57 -0400

    s3

--- packages/did_lambda/test/unit/test_s3.py
+++ packages/did_lambda/test/unit/test_s3.py
@@ -5,7 +5,13 @@ import pytest
 from did_lambda.utils import InfraError
 
 
-def test_put_object_sanitizes_failure(s3_module, monkeypatch):
+def test_put_object_sanitizes_failure(monkeypatch):
+    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-1")
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "test")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "test")
+
+    from did_lambda import s3 as s3_module
+
     bucket = "some_bucket"
     key = "some_key/eICR.xml"
     sensitive_failure = "some secret credentials or PII were rejected"
@@ -24,7 +30,7 @@ def test_put_object_sanitizes_failure(s3_module, monkeypatch):
     formatted_traceback = "".join(traceback.format_exception(exc.value))
 
     # verify that whatever caller caused the exception should NOT see PII in the exception
-    assert str(exc.value) == "S3 put_object failed"
+    assert str(exc.value) == f"S3 put_object failed s3://{bucket}/{key}"
 
     # assert none of the sensitive values are a substring in the traceback text
     assert sensitive_failure not in formatted_traceback
