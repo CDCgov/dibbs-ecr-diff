a9a549709e13f89a1897a4ccea8b24ae9bae677e
kevinfiol <me@kevinfiol.com>
2026-08-27 15:41:07 -0400

    wording

--- packages/did_lambda/test/unit/test_s3.py
+++ packages/did_lambda/test/unit/test_s3.py
@@ -6,8 +6,8 @@ from did_lambda.utils import InfraError
 
 
 def test_put_object_sanitizes_failure(s3_module, monkeypatch):
-    sensitive_bucket = "sensitive-aphl-bucket"
-    sensitive_key = "sensitive/eICR.xml"
+    bucket = "some_bucket"
+    key = "some_key/eICR.xml"
     sensitive_failure = "some secret credentials or PII were rejected"
 
     # make the underlying S3 client fail with sensitive error text
@@ -18,7 +18,7 @@ def test_put_object_sanitizes_failure(s3_module, monkeypatch):
     )
 
     with pytest.raises(InfraError) as exc:
-        s3_module.put_object(sensitive_bucket, sensitive_key, b"sensitive document")
+        s3_module.put_object(bucket, key, b"sensitive document")
 
     # join traceback list to a single string
     formatted_traceback = "".join(traceback.format_exception(exc.value))
@@ -27,5 +27,4 @@ def test_put_object_sanitizes_failure(s3_module, monkeypatch):
     assert str(exc.value) == "S3 put_object failed"
 
     # assert none of the sensitive values are a substring in the traceback text
-    for val in (sensitive_bucket, sensitive_key, sensitive_failure):
-        assert val not in formatted_traceback
+    assert sensitive_failure not in formatted_traceback
