6eade8c6ab1caea30a759468fb8cf6236240b9a9
kevinfiol <me@kevinfiol.com>
2026-08-28 15:51:05 -0400

    manifests should always return either True/False

--- packages/did_lambda/src/did_lambda/models.py
+++ packages/did_lambda/src/did_lambda/models.py
@@ -1,6 +1,7 @@
 """Models for S3 manifest files."""
 
 from datetime import datetime
+from typing import Literal
 
 from pydantic import BaseModel, Field
 
@@ -37,14 +38,14 @@ class DIDInputManifest(BaseModel):
 class DIDCompleteManifest(BaseModel):
     """A complete manifest containing DIDOutput files."""
 
-    did_skip: bool = Field(default=False, alias="DIDSkip")
+    did_skip: Literal[False] = Field(default=False, alias="DIDSkip")
     files: list[DIDOutputFile] = Field(alias="Files")
 
 
 class DIDCompleteErrorManifest(BaseModel):
     """A complete manifest in application-error cases."""
 
-    did_skip: bool = Field(default=True, alias="DIDSkip")
+    did_skip: Literal[True] = Field(default=True, alias="DIDSkip")
     error: str = Field(alias="Error")
 
 
